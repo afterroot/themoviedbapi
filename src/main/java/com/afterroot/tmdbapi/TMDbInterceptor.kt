205beecf132650c688f0c58064b6b4f571b0e8ea
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2022-11-24 23:00:01 +0530

    remove region parameter from TMDbInterceptor.kt

--- src/main/java/com/afterroot/tmdbapi/TMDbInterceptor.kt
+++ src/main/java/com/afterroot/tmdbapi/TMDbInterceptor.kt
@@ -20,13 +20,12 @@ import java.util.Locale
 /**
  * OkHttp interceptor that adds Tmdb api key in each request
  */
-class TMDbInterceptor(val key: String, val language: String = Locale.ENGLISH.toString(), val v4ApiKey: String? = null, val region: String) :
+class TMDbInterceptor(val key: String, val language: String = Locale.ENGLISH.toString(), val v4ApiKey: String? = null) :
     Interceptor {
     override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
         val req = chain.request()
         val url = req.url.newBuilder().addQueryParameter(Constants.PARAM_LANGUAGE, language)
             .addQueryParameter(Constants.PARAM_KEY, key)
-            .addQueryParameter(Constants.PARAM_REGION, region)
         val request = req.newBuilder().url(url.build())
         if (v4ApiKey != null) {
             request.addHeader("authorization", v4ApiKey)
